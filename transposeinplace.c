7ffd0741780c4d27a9de47cdeb4b116a4a7a8389
Devayanee Gupta <133389582+dg-2805@users.noreply.github.com>
2024-07-31 20:30:13 +0530

    Update transposeinplace.c

--- transposeinplace.c
+++ transposeinplace.c
@@ -13,27 +13,19 @@ void transpose(int **matrix, int n) {
 
 int main() {
     int n;
-    printf("Enter the size of the matrix (n): ");
+    printf("Enter the size of the matrix: ");
     scanf("%d", &n);
-
-    // Allocate memory for the matrix
     int **matrix = (int **)malloc(n * sizeof(int *));
     for (int i = 0; i < n; i++) {
         matrix[i] = (int *)malloc(n * sizeof(int));
     }
-
-    // Accept the matrix elements
     printf("Enter the elements of the matrix:\n");
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             scanf("%d", &matrix[i][j]);
         }
     }
-
-    // Transpose the matrix in place
     transpose(matrix, n);
-
-    // Display the transposed matrix
     printf("The transposed matrix is:\n");
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
@@ -41,8 +33,6 @@ int main() {
         }
         printf("\n");
     }
-
-    // Free the allocated memory
     for (int i = 0; i < n; i++) {
         free(matrix[i]);
     }
