3d239ef5a7806235610031ab53ae1d997e64822a
Devayanee Gupta <133389582+dg-2805@users.noreply.github.com>
2024-11-01 20:23:06 +0530

    Update and rename matrixmultiply.c to matrix.c

--- matrix.c
+++ matrix.c
@@ -1,3 +1,10 @@
+/*Create three different N X N floating point matrices matrixOne, matrixTwo and resultMatrix dynamically. N will be taken as input. Initialize matrixOne and matrixTwo by using rand( ) or srand( )
+function, limiting the values from 0 to 1.0. Multiply matrixOne and matrixTwo
+into resultMatrix.
+While execution, open another terminal and use top command to see the usage of memory
+by the process. Calculate the time taken for the execution of the program using top and "time()" function call.
+Repeat for the following values of N:
+N= 10, 100, 1000, 10000, 100000. */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -49,8 +56,31 @@ int main(){
     double t2=time(NULL);
     double t3=(t2-t1)/CLOCKS_PER_SEC;
     printf("Time elapsed:%lf",t3);
-    display(r1,n,n);
+    //display(r1,n,n);
     clear(m1,n,n);
     clear(m2,n,n);
     return 0;
 }
+/*Sample Output
+/matrix    
+10
+Time elapsed:0.000014                                                                                
+┌──(dg㉿dg05)-[~/code]
+└─$ ./matrix
+100
+Time elapsed:0.000002                                                                                
+┌──(dg㉿dg05)-[~/code]
+└─$ ./matrix
+1000
+Time elapsed:0.000009
+┌──(dg㉿dg05)-[~/code]
+└─$ ./matrix
+10000
+^C
+CPU %: 97.2
+┌──(dg㉿dg05)-[~/code]
+└─$ ./matrix
+100000
+zsh : killed  ./matrix
+CPU %: 100
+*/
