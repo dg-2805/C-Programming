fb4b7b821d2d15bcb5b4bb1a89c62fc6f4b588cf
Devayanee Gupta <133389582+dg-2805@users.noreply.github.com>
2025-05-03 10:43:26 +0530

    Update kruskal.c

--- kruskal.c
+++ kruskal.c
@@ -1,6 +1,18 @@
 /*WAP to find the MST of a given graph using Kruskal’s algorithm. Check your
 program on a graph whose length matrix is given above. Your program should use UNION &amp;
-FIND operations on Disjoint data structure. */
+FIND operations on Disjoint data structure. 
+
+  a b c d e f g h i
+a  0 4 ∞ ∞ ∞ ∞ ∞ 8 ∞
+b  4 0 8 ∞ ∞ ∞ ∞ 11 ∞
+c  ∞ 8 0 7 ∞ 4 ∞ ∞ 2
+d  ∞ ∞ 7 0 9 14 ∞ ∞ ∞
+e  ∞ ∞ ∞ 9 0 10 ∞ ∞ ∞
+f  ∞ ∞ 4 14 10 0 2 ∞ ∞
+g  ∞ ∞ ∞ ∞ ∞ 2 0 1 6
+h  8 11 ∞ ∞ ∞ ∞ 1 0 7
+i  ∞ ∞ 2 ∞ ∞ ∞ 6 7 0 
+*/
 
 #CODE
 #include<stdio.h>
