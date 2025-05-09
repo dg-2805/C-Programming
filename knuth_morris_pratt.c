9488985951c812246b2bf00449f9f4cddabd745f
Devayanee Gupta <133389582+dg-2805@users.noreply.github.com>
2025-05-09 22:35:42 +0530

    Update knuth_morris_pratt.c

--- knuth_morris_pratt.c
+++ knuth_morris_pratt.c
@@ -1,5 +1,5 @@
 /*Write a program to implement Knuth-Morris-Pratt (KMP) algorithm for pattern matching. Check your
-program for the following Text &amp; Pattern:
+program for the following Text & Pattern:
 T: b a c b a b a b a c a b a b a c a
 P: a b a b a c a */
 
