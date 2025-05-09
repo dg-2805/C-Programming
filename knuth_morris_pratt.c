ab2c88417afaf00f50e40955dce0a0a7bf0ad061
Devayanee Gupta <133389582+dg-2805@users.noreply.github.com>
2025-05-09 22:03:24 +0530

    Create knuth_morris_pratt.c

---
+++ knuth_morris_pratt.c
@@ -0,0 +1,70 @@
+/*Write a program to implement Knuth-Morris-Pratt (KMP) algorithm for pattern matching. Check your
+program for the following Text &amp; Pattern:
+T: b a c b a b a b a c a b a b a c a
+P: a b a b a c a */
+
+#CODE
+#include <stdio.h> 
+#include <string.h> 
+ 
+// Function to compute the LPS (Longest Prefix Suffix) array 
+void computeLPSArray(char* pattern, int M, int* lps) { 
+    int length = 0; 
+    lps[0] = 0; // LPS of first character is always 0 
+ 
+    int i = 1; 
+    while (i < M) { 
+        if (pattern[i] == pattern[length]) { 
+            length++; 
+            lps[i] = length; 
+            i++; 
+        } else { 
+            if (length != 0) { 
+                length = lps[length - 1]; 
+            } else { 
+                lps[i] = 0; 
+                i++; 
+            } 
+        } 
+    } 
+} 
+ 
+// KMP search function 
+void KMPSearch(char* pattern, char* text) { 
+    int M = strlen(pattern); 
+    int N = strlen(text); 
+ 
+    int lps[M]; 
+    computeLPSArray(pattern, M, lps); 
+ 
+    int i = 0; // index for text[] 
+    int j = 0; // index for pattern[] 
+    while (i < N) { 
+        if (pattern[j] == text[i]) { 
+            i++; 
+            j++; 
+        } 
+ 
+        if (j == M) { 
+            printf("Pattern found at index %d\n", i - j); 
+            j = lps[j - 1]; // Look for next possible match 
+        } else if (i < N && pattern[j] != text[i]) { 
+            if (j != 0) { 
+                j = lps[j - 1]; 
+            } else { 
+                i++; 
+            } 
+        } 
+    } 
+} 
+
+int main() { 
+    char text[] = "BACBABABACABABACA"; 
+    char pattern[] = "ABABACA"; 
+    KMPSearch(pattern, text); 
+    return 0; 
+}
+
+/*OUTPUT
+Pattern found at index 4
+Pattern found at index 10 */
